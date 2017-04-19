3b3397173a9687dd19766d716c464eb0e413b580
Eduard Trias <triaseduard@gmail.com>
2017-04-19 10:25:04 +0200

    Refactor in process
    Moving var to constructor and working in reset function

--- logic.js
+++ logic.js
@@ -7,6 +7,15 @@ function Game(num){
   this.count = 3;
   this.decimasTimer;
   this.isOn = false;
+  this.decimas = 0; //Not checked
+  this.seconds = 0;
+  this.minutes = 0;
+
+  $(".gridContainer").append($("<div>").addClass("info"));
+  $(".info").append($("<p>Press Start and count until 50 as fast as you can!</p>").addClass("infoP"));
+  $(".info").append($("<p></p>").addClass("countdownNum"));
+  this.infoP = $(".infoP");
+  this.countdownNum = $(".countdownNum");
 
   for (var rowIndex = 0; rowIndex < this.rows ; rowIndex++) {
     for (var colIndex = 0; colIndex < this.col; colIndex++) {
@@ -15,22 +24,21 @@ function Game(num){
     .append($("<p>")
     .addClass("cellNumber")));
 
-  this.cellNumber = document.querySelectorAll(".cellNumber");
-  this.cell = document.querySelectorAll(".cell");
+
     }
   }
-  //this.message = document.querySelector("#info p"); //not sure it will be necessary
 
+  this.cellNumber = document.querySelectorAll(".cellNumber");
+  this.cell = document.querySelectorAll(".cell");
 
-  $(".gridContainer").append($("<div>").addClass("info"));
-  $(".info").append($("<p>Press Start and count until 50 as fast as you can!</p>").addClass("infoP"));
-  $(".gridContainer").append($("<p>").addClass("countdownNum"));
-  this.infoP = $(".infoP");
-  this.countdownNum = $(".countdownNum");
-  //this.pressStartMessage = document.querySelector(".info");
-  //this.pressStartMessage.textContent = "Press Start and count until 50 as fast as you can!";
+  this.minutesText = document.querySelector("#minutes"); //Not checked
+  this.secondsText = document.querySelector("#seconds");
+  this.decimasText = document.querySelector("#decimas");
+
+  this.nextNumOne = document.querySelector("#nextNumOne");
+  this.nextNumTwo = document.querySelector("#nextNumTwo");
+
 }
-
 
 //----------Create Array In Order from 1 to 25------------------
 Game.prototype.createArrInOrder = function() {
@@ -74,13 +82,13 @@ Game.prototype.displayNums = function() {
 //-------------------Click number actions---------------------------
 Game.prototype.clickNumber = function() {
 //var showNum = 1;
-var self = this;
+var that = this;
 
-var nextNumOne = document.querySelector("#nextNumOne");
-var nextNumTwo = document.querySelector("#nextNumTwo");
+// var nextNumOne = document.querySelector("#nextNumOne");
+// var nextNumTwo = document.querySelector("#nextNumTwo");
 
-nextNumOne.textContent = "1";
-nextNumTwo.textContent = "2";
+that.nextNumOne.textContent = "1";
+that.nextNumTwo.textContent = "2";
 
 for(var i = 0; i < this.cell.length; i++) {
   this.cellNumber[i].addEventListener("click", function() {
@@ -88,43 +96,43 @@ for(var i = 0; i < this.cell.length; i++) {
     this.innerHTML = Number(this.innerHTML);
 
     //still to improve the else part
-    if (this.innerHTML == self.checkNum && this.innerHTML < 26) {
+    if (this.innerHTML == that.checkNum && this.innerHTML < 26) {
 
       this.parentNode.classList.add("cellRight");
       this.parentNode.classList.remove("cell");
-      this.textContent = self.arrShuffle[self.checkNum + 24];
-      nextNumOne.textContent = self.chekNum;
-      self.checkNum ++;
-      console.log(self.checkNum);
-      nextNumOne.textContent = self.checkNum;
-      nextNumTwo.textContent = self.checkNum + 1;
+      this.textContent = that.arrShuffle[that.checkNum + 24];
+      that.nextNumOne.textContent = that.chekNum;
+      that.checkNum ++;
+      console.log(that.checkNum);
+      that.nextNumOne.textContent = that.checkNum;
+      that.nextNumTwo.textContent = that.checkNum + 1;
         //$('.cellNumber').append('<p>' + game.checkNum + '</p>')
-    }  else if (this.innerHTML == self.checkNum && this.innerHTML > 25) {
+    }  else if (this.innerHTML == that.checkNum && this.innerHTML > 25) {
         this.parentNode.classList.add("cellDone");
         this.parentNode.classList.remove("cellRight");
 
-        if (self.checkNum === 50) {
-          console.log("pop");
-          clearInterval(self.decimasTimer);
+        if (that.checkNum === 50) {
+          //console.log("pop");
+          clearInterval(that.decimasTimer);
         }
 
-        self.checkNum ++;
-        console.log(self.checkNum);
+        that.checkNum ++;
+        console.log(that.checkNum);
 
-        if (self.checkNum < 50) {
-          nextNumOne.textContent = self.checkNum;
-          nextNumTwo.textContent = self.checkNum + 1;
+        if (that.checkNum < 50) {
+          that.nextNumOne.textContent = that.checkNum;
+          that.nextNumTwo.textContent = that.checkNum + 1;
         }
 
-        if (self.checkNum === 50) {
-          nextNumOne.textContent = self.checkNum;
-          console.log("Box 2 Empty");
-          nextNumTwo.textContent = "";
+        if (that.checkNum === 50) {
+          that.nextNumOne.textContent = that.checkNum;
+          //console.log("Box 2 Empty");
+          that.nextNumTwo.textContent = "";
         }
 
-        if (self.checkNum === 51) {
-          console.log("textNumOne Empty");
-          nextNumOne.textContent = "";
+        if (that.checkNum === 51) {
+          //console.log("textNumOne Empty");
+          that.nextNumOne.textContent = "";
         }
 
       }
@@ -140,20 +148,22 @@ Game.prototype.countdown = function() {
   this.infoP.remove();
   //var countdownNum = document.querySelector(".countdownNum")[0];
 
-  var self = this;
+  var that = this;
 
   var countToStart = setInterval(function () {
 
-        if (self.count > 0){
-          self.countdownNum.textContent = self.count;
-
-          console.log(self.count);
-          self.count --;
-        } else if (self.count === 0) {
-          self.countdownNum.textContent = "go!";
-          self.count --;
-        }  else if (self.count === -1) {
-          document.querySelector(".info").remove();
+        if (that.count > 0){
+          //$(".countdownNum").html(that.count);
+          that.countdownNum[0].textContent = that.count;
+          //console.log(that.countdownNum.innerHTML);
+          //console.log(that.countdownNum);
+          that.count --;
+        } else if (that.count === 0) {
+          that.countdownNum[0].textContent = "go!";
+          //console.log(that.countdownNum.textContent);
+          that.count --;
+        }  else if (that.count === -1) {
+          document.querySelector(".info").style.visibility = "hidden"; //remove();
           clearInterval(countToStart);
         }
       }, 1000)
@@ -163,41 +173,77 @@ Game.prototype.countdown = function() {
 //-----------------------timer-----------------------------
 Game.prototype.timer = function() {
 
-  var minutesText = document.querySelector("#minutes");
-  var secondsText = document.querySelector("#seconds");
-  var decimasText = document.querySelector("#decimas");
+  // var minutesText = document.querySelector("#minutes");
+  // var secondsText = document.querySelector("#seconds");
+  // var decimasText = document.querySelector("#decimas");
 
-  var decimas = 0;
-  var seconds = 0;
-  var minutes = 0;
+  // var decimas = 0;
+  // var seconds = 0;
+  // var minutes = 0;
+
+  var that = this;
 
 this.decimasTimer = setInterval(function() {
 
-  if (decimas < 10) {
-    decimasText.textContent = decimas;
-    secondsText.textContent = seconds + ":";
-    minutesText.textContent = minutes + ":";
+  if (that.decimas < 10) {
+    that.decimasText.textContent = that.decimas;
+    that.secondsText.textContent = that.seconds + ":";
+    that.minutesText.textContent = that.minutes + ":";
 
-    decimas ++;
-    if (decimas === 10) {
-      decimas = 0;
-      seconds++;
+    that.decimas ++;
+    if (that.decimas === 10) {
+      that.decimas = 0;
+      that.seconds++;
     }
-      if (seconds === 60) {
-        seconds = 0;
-        minutes++;
+      if (that.seconds === 60) {
+        that.seconds = 0;
+        that.minutes++;
         }
         // if (this.checkNum === 51) { //doesn't work!!
         //
         // }
-          if (minutes === 1) {
+          if (that.minutes === 5) {
             // decimas = 0;
             // seconds = 0;
             // minutes = 0;
-            clearInterval(this.decimasTimer);
+            clearInterval(that.decimasTimer);
           }
   }
 }, 100)
+
+}
+
+
+//-------------------Reset Global values-------------------
+
+Game.prototype.resetGlobalValues = function() {
+
+  var that = this;
+
+  this.arrInOrder = [];
+  this.arrShuffle = [];
+
+  this.count = 3;
+
+  this.checkNum = 1;
+
+  clearInterval(that.decimasTimer);
+  this.decimas = 0;
+  this.seconds = 0;
+  this.minutes = 0;
+
+  this.nextNumOne = "";
+  this.nextNumTwo = "";
+
+  console.log("nextNum reset" + this.nextNumOne);
+
+}
+
+//-------------------Reset HTML---------------------------
+
+Game.prototype.resetHTML = function() {
+
+  document.querySelector(".info").style.visibility = "visible";
 
 }
 
@@ -206,11 +252,13 @@ this.decimasTimer = setInterval(function() {
 
 Game.prototype.resetGame = function() {
 
-  this.checkNum = 1;
-  this.count = 3;
-  console.log(this.checkNum);
-  console.log(this.count);
-  var message = document.querySelector("#info p").style.visibility = "visible";
+  game.resetGlobalValues()
+
+
+  // console.log(this.checkNum);
+  // console.log(this.count);
+  //var message = document.querySelector("#info p").style.visibility = "visible";
+  //$(".gridContainer").append($("<div>").addClass("info")); //try
 
 
 }
@@ -237,7 +285,7 @@ $(document).ready(function() {
     onOffButton.addEventListener("click", function() {
 
       if (that.isOn === false) {
-        console.log(game.checkNum);
+        //console.log(game.checkNum);
         game.countdown();
         setTimeout(function() { game.timer(); }, 5000);
         game.createArrInOrder();
@@ -251,7 +299,7 @@ $(document).ready(function() {
         buttonAround.style.visibility = "hidden";
         this.textContent = "Reset";
         setTimeout(function(){
-          console.log("pop");
+          //console.log("pop");
           this.onOffButton.style.visibility = "visible";
           buttonAround.style.visibility = "visible";
         }, 5000);
@@ -261,7 +309,7 @@ $(document).ready(function() {
           game.resetGame();
 
 
-        //that.isOn = !that.isOn;
+        that.isOn = !that.isOn;
 
       }
 
@@ -270,5 +318,4 @@ $(document).ready(function() {
   };
 
   game.startGame();
-
 });
